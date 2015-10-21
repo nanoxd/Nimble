df859a7585e61ed6107d52029d522825e3ff961d
Fernando Paredes <nano@fdp.io>
2015-10-21 14:07:28 -0700

    Add imports for Obj-C files to umbrella header

--- Nimble/Nimble.h
+++ Nimble/Nimble.h
@@ -1,4 +1,6 @@
 #import <Foundation/Foundation.h>
+#import <Nimble/DSL.h>
+#import <Nimble/NMBExceptionCapture.h>
 
 FOUNDATION_EXPORT double NimbleVersionNumber;
 FOUNDATION_EXPORT const unsigned char NimbleVersionString[];
